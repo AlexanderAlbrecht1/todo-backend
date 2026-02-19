94e06d3b5050fefa7e1e062b3478132be1f732a9
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2026-02-19 22:02:47 +0100

    get all todos

--- src/main/java/de/alex/ToDoResource.java
+++ src/main/java/de/alex/ToDoResource.java
@@ -1,4 +1,23 @@
 package de.alex;
 
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@ApplicationScoped
+@Path("/todo")
 public class ToDoResource {
+
+@GET
+@Produces(MediaType.APPLICATION_JSON)
+    public List<ToDoEntity> getAllToDos() {
+    return ToDoEntity.listAll();
+}
+
+
 }
