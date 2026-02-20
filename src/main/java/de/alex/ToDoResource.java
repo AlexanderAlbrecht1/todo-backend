7a0269045d9bbaeba6f579de109a187dfd74b8b7
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2026-02-20 12:29:47 +0100

    post new todo works

--- src/main/java/de/alex/ToDoResource.java
+++ src/main/java/de/alex/ToDoResource.java
@@ -11,21 +11,24 @@ import java.util.List;
 
 @ApplicationScoped
 @Path("/todo")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+
 public class ToDoResource {
 
 @GET
-@Produces(MediaType.APPLICATION_JSON)
+
     public List<ToDoEntity> getAllToDos() {
     return ToDoEntity.listAll();
 }
 
 @POST
 @Transactional
-@Consumes(MediaType.APPLICATION_JSON)
+
     public Response createToDo(ToDoEntity toDoEntity) {
-    toDoEntity.persist();
-    return Response.status(Response.Status.CREATED).entity(toDoEntity).build();
-}
+        toDoEntity.persist();
+        return Response.status(Response.Status.CREATED).entity(toDoEntity).build();
+    }
 
 
 
