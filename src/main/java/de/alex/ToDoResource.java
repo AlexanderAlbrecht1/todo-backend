62a66c784921bd7b6b004e16a2fe97a28143d99e
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2026-02-20 13:02:09 +0100

    get single todo

--- src/main/java/de/alex/ToDoResource.java
+++ src/main/java/de/alex/ToDoResource.java
@@ -1,11 +1,12 @@
 package de.alex;
 
-
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
 
 import java.util.List;
 
@@ -17,19 +18,22 @@ import java.util.List;
 public class ToDoResource {
 
 @GET
-
     public List<ToDoEntity> getAllToDos() {
-    return ToDoEntity.listAll();
-}
+        return ToDoEntity.listAll();
+    }
+
+@GET
+@Path("/{id}")
+    public ToDoEntity getSingleToDo(@PathParam("id") Long id) {
+        ToDoEntity entity = ToDoEntity.findById(id);
+        return entity;
+    }
 
 @POST
 @Transactional
-
     public Response createToDo(ToDoEntity toDoEntity) {
         toDoEntity.persist();
         return Response.status(Response.Status.CREATED).entity(toDoEntity).build();
     }
-
-
 
 }
